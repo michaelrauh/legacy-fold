d2221fdcfc2cde87378e7731ccbcaced867317e8
Michael Rauh <michaelrauh@ymail.com>
2013-06-09 22:42:36 -0400

    ported to c# under set intersect design

---
+++ linearfold/fold/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+            var trees = new Trees();
+            trees.load();
+
+            SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>.KeyCollection aKeys = trees.Data.Keys;
+
+            foreach (string a in aKeys) // each of the roots
+            {
+                SortedDictionary<string, SortedSet<string>>.KeyCollection bdKeys = trees.Data[a].Keys;
+                string[] childrenOfA = bdKeys.ToArray<string>();
+
+                for (int currentB = 0; currentB < bdKeys.Count; currentB++) // for each child of a (b)
+                {
+                    for (int currentD = currentB; currentD < bdKeys.Count; currentD++) //for each unused child of a (d)
+                    {
+                        string b = childrenOfA[currentB];
+                        string d = childrenOfA[currentD];
+                        SortedDictionary<string, SortedSet<string>> childrenOfB = trees.Data[b];
+                        SortedDictionary<string, SortedSet<string>> childrenOfD = trees.Data[d];
+
+                        SortedDictionary<string, SortedSet<string>>.KeyCollection childrenOfBKeys = childrenOfB.Keys;
+                        SortedDictionary<string, SortedSet<string>>.KeyCollection childrenOfDKeys = childrenOfD.Keys;
+
+                        var allEValues = childrenOfBKeys.Intersect(childrenOfDKeys);
+
+                        foreach (string e in allEValues) // for each e value (child of b and d)
+                        {
+                            SortedSet<string> allGValues = trees.Data[a][d];
+
+                            foreach (string g in allGValues) // for each g value (child of ad)
+                            {
+                                SortedSet <string> childrenOfBE = trees.Data[b][e];
+
+                                SortedDictionary<string, SortedSet<string>> childrenOfG = trees.Data[g];
+                                SortedDictionary<string, SortedSet<string>>.KeyCollection childrenOfGKeys = childrenOfG.Keys;
+
+                                var allHValues = childrenOfGKeys.Intersect(childrenOfBE);
+
+                                foreach (string h in allHValues) //for each h value (child of be and g)
+                                {
+                                    SortedSet<string> allCValues = trees.Data[a][b];
+
+                                    foreach (string c in allCValues) // for each c value (child of ab)
+                                    {
+                                        SortedDictionary<string, SortedSet<string>> childrenOfC = trees.Data[c];
+                                        SortedDictionary<string, SortedSet<string>>.KeyCollection childrenOfCKeys = childrenOfC.Keys;
+
+                                        SortedSet<string> childrenOfDE = trees.Data[d][e];
+
+                                        var AllFValues = childrenOfCKeys.Intersect(childrenOfDE);
+
+                                        foreach (string f in AllFValues) //for each f value (child of c and de)
+                                        {
+                                            SortedSet<string> childrenOfCF = trees.Data[c][f];
+                                            SortedSet<string> childrenOfGH = trees.Data[g][h];
+
+                                            var allIValues = childrenOfCF.Intersect(childrenOfGH);
+
+                                            foreach (string i in allIValues)
+                                            {
+                                                string[] results = new[] {a,b,c,d,e,f,g,h,i};
+
+                                                var duplicates = results.GroupBy(s => s).Where(x => x.Count() > 1).Select(x => x.Key);
+                                                if (duplicates.Count()==0)
+                                                {
+                                                    foreach (string str in results)
+                                                    {
+                                                        System.Console.WriteLine(str);
+                                                    }
+                                                    System.Console.WriteLine("////////////");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            timer.Stop();
+            System.Diagnostics.Debug.WriteLine("Elapsed Time {0}", timer.Elapsed);
+            Console.Read();
+        }
+    }
+}
---
+++ linearfold/fold/load.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fold
+{
+    class Trees
+    {
+        private SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> data = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+        public SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> Data
+        {
+            get { return data; }
+        }
+        public SortedDictionary<string, SortedSet<string>> this[string index]
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(index))
+                {
+                    throw new ArgumentNullException("index");
+                }
+                if (data.ContainsKey(index))
+                {
+                return data[index]; 
+                }
+                else
+                {
+                    throw new ArgumentException("tree does not contain an entry for " + index, "index");
+                }
+            }
+        }
+
+        public SortedSet<string> this[string index,string index2]
+        {
+            get 
+            {
+                if (data.ContainsKey(index))
+                {
+                    var x = data[index];
+                    if (x.ContainsKey(index2))
+                    {
+                        return x[index2];
+                    }
+                    else
+                    {
+                        throw new ArgumentException("tree does not contain an entry for " + index2, "index2");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("tree does not contain an entry for " + index, "index");
+                }
+            }
+        }
+        public void load()
+        {
+            string[] words = System.IO.File.ReadAllText(@"C:\Users\Owner\Desktop\mars.txt").Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!data.ContainsKey(words[i])) // if the top is missing
+                {
+                    SortedSet<string> bottom = new SortedSet<string>();
+                    if (i + 2 < words.Length) // if boundary conditions fail a dummy is added to preserve structure
+                        bottom.Add(words[i + 2]);
+
+                    SortedDictionary<string, SortedSet<string>> middle = new SortedDictionary<string, SortedSet<string>>();
+                    if (i + 1 < words.Length)
+                        middle.Add(words[i + 1], bottom);
+
+                    data.Add(words[i], middle);
+                }
+                else
+                {
+                    if (!data[words[i]].ContainsKey(words[i + 1])) //if the branch is missing
+                    {
+                        SortedSet<string> bottom = new SortedSet<string>();
+                        if (i + 2 < words.Length)
+                            bottom.Add(words[i + 2]);
+
+                        data[words[i]].Add(words[i + 1], bottom);
+                    }
+                    else
+                    {
+                        if (!data[words[i]][words[i + 1]].Contains(words[i + 2])) //if the leaf is missing
+                        {
+                            if (i+2 < words.Length)
+                            data[words[i]][words[i + 1]].Add(words[i + 2]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
